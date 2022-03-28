9931d5a2784793830136620d8b2e30e2f4ac10d1
clx <clx.@qq.com>
2022-03-28 21:10:42 +0800

    five init Git(hot-fix)

--- src/main/java/com/clx/git/GitTest.java
+++ src/main/java/com/clx/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("This is Two Git Test");
         System.out.println("This is Third Git Test");
+        System.out.println("This is five Git Test(hot-fix)");
     }
 }
