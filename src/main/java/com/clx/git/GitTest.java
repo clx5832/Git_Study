694ce27cab876a5b37203ebb1665515eb7cfc407
clx <clx.@qq.com>
2022-03-28 20:16:39 +0800

    First init Git

---
+++ src/main/java/com/clx/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.clx.git;
+
+/**
+ * @author CLX
+ * @create 2022-03-28 20:14
+ * @desc
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("This is Git Test");
+    }
+}
